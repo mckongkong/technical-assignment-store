b166cad539a1876e3d2e81af57be0025deb3508c
Marc Christian Kong Kong <marc-christian.kongkong@contentsquare.com>
2024-05-16 16:47:47 +0200

    first implementation

--- src/store.ts
+++ src/store.ts
@@ -1,14 +1,18 @@
 import { JSONArray, JSONObject, JSONPrimitive } from "./json-types";
 
+import "reflect-metadata";
+
+import {get, set} from 'lodash';
+
 export type Permission = "r" | "w" | "rw" | "none";
 
 export type StoreResult = Store | JSONPrimitive | undefined;
 
 export type StoreValue =
-  | JSONObject
-  | JSONArray
-  | StoreResult
-  | (() => StoreResult);
+| JSONObject
+| JSONArray
+| StoreResult
+| (() => StoreResult);
 
 export interface IStore {
   defaultPolicy: Permission;
@@ -20,33 +24,90 @@ export interface IStore {
   entries(): JSONObject;
 }
 
-export function Restrict(...params: unknown[]): any {
+const restrictMetadataKey = Symbol("restrict");
+
+export function Restrict(permission?: Permission): any {
+  return Reflect.metadata(restrictMetadataKey, permission ?? 'none')
 }
 
 export class Store implements IStore {
   defaultPolicy: Permission = "rw";
 
+  data: JSONObject = {}
+
+
   allowedToRead(key: string): boolean {
-    throw new Error("Method not implemented.");
+    const restricton = Reflect.getMetadata(restrictMetadataKey,this,key)
+    
+    if(!restricton){
+      return this.defaultPolicy !== 'none'
+    }else{
+      return restricton !== 'none'
+    }
   }
 
   allowedToWrite(key: string): boolean {
-    throw new Error("Method not implemented.");
+    const restricton = Reflect.getMetadata(restrictMetadataKey,this,key)
+    if(!restricton){
+      return this.defaultPolicy === 'rw'
+    }else{
+      return restricton === 'rw'
+    }
   }
 
   read(path: string): StoreResult {
-    throw new Error("Method not implemented.");
+    const keys = path.split(':')
+    const arrayOfKeys: string[] = [];    
+    let callerObject = Object.create(this)
+
+    for(const key of keys){
+      arrayOfKeys.push(key);
+      
+      if(!callerObject.allowedToRead(key)){
+        throw new Error(`Cannot read ${key}`)
+      }
+      if(get(this,arrayOfKeys) instanceof Store){
+        callerObject = Object.create(get(this,arrayOfKeys));
+      }
+      
+    }
+
+    return get(this,arrayOfKeys) as StoreResult;
   }
 
   write(path: string, value: StoreValue): StoreValue {
-    throw new Error("Method not implemented.");
+     const keys = path.split(':')
+    const arrayOfKeys: string[] = [];
+    let callerObject = Object.create(this)
+
+    for(const key of keys){
+      arrayOfKeys.push(key);
+      if(keys.length !== arrayOfKeys.length){
+        if(!callerObject.allowedToRead(key)){
+          throw new Error(`Cannot read ${key}`)
+        }
+        if(get(this, arrayOfKeys) instanceof Store){
+          callerObject = Object.create(get(this, arrayOfKeys))
+        }
+      }else{
+        if(!callerObject.allowedToWrite(key)){
+          throw new Error(`Cannot write ${key}`)
+        }
+      }
+      
+    }
+   
+    return set(this,arrayOfKeys,value) as StoreValue
   }
 
   writeEntries(entries: JSONObject): void {
-    throw new Error("Method not implemented.");
+    const keys = Object.keys(entries);
+    for(const key of keys){
+      this.write(key,entries[key])
+    }
   }
 
   entries(): JSONObject {
-    throw new Error("Method not implemented.");
+    return this.data;
   }
 }
