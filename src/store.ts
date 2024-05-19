9b84839c0932e3b858a771acc7d1525e416890a0
Marc Christian Kong Kong <marc-christian.kongkong@contentsquare.com>
2024-05-19 09:03:21 +0200

    full implementation

--- src/store.ts
+++ src/store.ts
@@ -33,12 +33,8 @@ export function Restrict(permission?: Permission): any {
 export class Store implements IStore {
   defaultPolicy: Permission = "rw";
 
-  data: JSONObject = {}
-
-
   allowedToRead(key: string): boolean {
     const restricton = Reflect.getMetadata(restrictMetadataKey,this,key)
-    
     if(!restricton){
       return this.defaultPolicy !== 'none'
     }else{
@@ -57,47 +53,62 @@ export class Store implements IStore {
 
   read(path: string): StoreResult {
     const keys = path.split(':')
-    const arrayOfKeys: string[] = [];    
+    let arrayOfKeys: string[] = [];    
     let callerObject = Object.create(this)
 
     for(const key of keys){
       arrayOfKeys.push(key);
-      
+
       if(!callerObject.allowedToRead(key)){
-        throw new Error(`Cannot read ${key}`)
-      }
-      if(get(this,arrayOfKeys) instanceof Store){
-        callerObject = Object.create(get(this,arrayOfKeys));
+        throw new Error(`Cannot read property ${key}`)
       }
       
+      const currentValue = get(this,arrayOfKeys)
+
+      if(currentValue instanceof Store){
+        callerObject = Object.create(currentValue);
+        arrayOfKeys = []
+      } else if(typeof currentValue == 'function'){
+        callerObject = Object.create(currentValue())
+        arrayOfKeys = []
+      }
     }
 
-    return get(this,arrayOfKeys) as StoreResult;
+    let result;
+    const finalValue = get(callerObject,arrayOfKeys)
+
+    if((typeof finalValue === 'object' || finalValue == undefined)){
+      result = Object.create(callerObject);
+      Object.assign(result,finalValue)
+      set(this,keys,result)
+    } else {
+      result = finalValue
+    }
+
+    return result;
   }
 
   write(path: string, value: StoreValue): StoreValue {
-     const keys = path.split(':')
-    const arrayOfKeys: string[] = [];
+    const keys = path.split(':')
+    let arrayOfKeys: string[] = [];
     let callerObject = Object.create(this)
 
     for(const key of keys){
       arrayOfKeys.push(key);
       if(keys.length !== arrayOfKeys.length){
         if(!callerObject.allowedToRead(key)){
-          throw new Error(`Cannot read ${key}`)
+          throw new Error(`Cannot read property ${key}`)
         }
         if(get(this, arrayOfKeys) instanceof Store){
-          callerObject = Object.create(get(this, arrayOfKeys))
+          callerObject = Object.create(get(this, arrayOfKeys));
         }
       }else{
         if(!callerObject.allowedToWrite(key)){
-          throw new Error(`Cannot write ${key}`)
+          throw new Error(`Cannot write property ${key}`)
         }
       }
-      
     }
-   
-    return set(this,arrayOfKeys,value) as StoreValue
+    return set(this,arrayOfKeys,value)
   }
 
   writeEntries(entries: JSONObject): void {
@@ -108,6 +119,14 @@ export class Store implements IStore {
   }
 
   entries(): JSONObject {
-    return this.data;
+    const keys = Object.keys(this);
+    keys.splice(keys.indexOf('defaultPolicy'), 1)
+    const obj: JSONObject = {}
+    for(const key of keys){
+      if(this.allowedToRead(key)){
+        obj[key] = (this as any)[key]
+      }
+    }
+    return obj;
   }
 }
